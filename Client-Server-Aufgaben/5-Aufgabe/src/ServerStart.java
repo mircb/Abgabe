3014593ed7ac2f45fe231b0832e4636764fa22ad
mircb <mirc.bo@gmail.com>
2017-03-05 12:35:19 +0100

    Aufgabe-5 Komplett

--- Client-Server-Aufgaben/5-Aufgabe/src/ServerStart.java
+++ Client-Server-Aufgaben/5-Aufgabe/src/ServerStart.java
@@ -3,7 +3,7 @@
  */
 public class ServerStart {
 
-    public static void main(String[] args) {        //Klasse zum startet des Servers
+    public static void main(String[] args) {     //Klasse zum startet des Servers
 
         Server server = new Server();
 
