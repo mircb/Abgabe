c60ecfe4719abc864db91113c9e4f9634799f7a9
mircb <mirc.bo@gmail.com>
2017-05-30 12:34:11 +0200

    Soap- Anpassungen

--- SOAP-ubung/src/webservice/MyClient.java
+++ SOAP-ubung/src/webservice/MyClient.java
@@ -8,16 +8,14 @@ import webservice.gen.mywebserviceservice.MyWebServiceService;
 public class MyClient {
 
 	public static void main(String[] args){
-		System.out.println("Java mit eigenem Server und useWSImport.bat:\n-------------------");
-		System.out.println("Welche Operation?");
+		//System.out.println("Java mit eigenem Server und useWSImport.bat:\n-------------------");
+		System.out.println("Welche Operation?(+ - * / )");
 		Scanner scanner = new Scanner(System.in);
 		String input = scanner.nextLine();
 		
 		MINICALC service = new MyWebServiceService().getMINICALCPort();
 
 
-
-		
 		System.out.println("Gib die erste Zahl ein:");
 		String f1 = scanner.nextLine();
 		
@@ -30,16 +28,17 @@ public class MyClient {
 		}
 		if( input.equals("-")) {
 
-			System.out.println("Das Ergebniss ist: " + service.addition(Float.parseFloat(f1), Float.parseFloat(f2)));
+			System.out.println("Das Ergebniss ist: " + service.subbtraktion(Float.parseFloat(f1), Float.parseFloat(f2)));
 		}
 		if( input.equals("*")) {
 
-			System.out.println("Das Ergebniss ist: " + service.addition(Float.parseFloat(f1), Float.parseFloat(f2)));
+			System.out.println("Das Ergebniss ist: " + service.multiplikation(Float.parseFloat(f1), Float.parseFloat(f2)));
 		}
 		if( input.equals("/")) {
 
-			System.out.println("Das Ergebniss ist: " + service.addition(Float.parseFloat(f1), Float.parseFloat(f2)));
+			System.out.println("Das Ergebniss ist: " + service.division(Float.parseFloat(f1), Float.parseFloat(f2)));
 		}
-		scanner.close();
+		}
 	}
-}
+
+
